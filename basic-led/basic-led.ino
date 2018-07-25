d77253384ec030defdae4e79ff2e1971eeb59031
Joshua Rasmussen <joshrasmussen@Joshuas-MacBook-Pro.local>
2018-07-25 12:03:10 -0700

    Update project to use makefiles

--- basic-led/basic-led.ino
+++ basic-led/basic-led.ino
@@ -1,16 +1,17 @@
-#define LED_PIN 2
-#define SWITCH_PIN 3
-
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(SWITCH_PIN, INPUT);
-
-  Serial.begin(9600);
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
-  int value = digitalRead(SWITCH_PIN);
-  digitalWrite(LED_PIN, value);   
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);                       // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
 }
